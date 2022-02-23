701870d248c361118fdaab6f10cdbb47d14b5dd0
beefchimi <curtdulmage@gmail.com>
2022-02-23 09:51:08 -0500

    :wrench: [Vite] Adjust config

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Vite CSS Modules
 
 This repo exists to test use of CSS Modules in Vite.
+
+## Instructions
+
+```sh
+git clone git@github.com:beefchimi/vite-css-modules.git
+cd vite-css-modules
+npm install
+npm run start
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import {defineConfig} from 'vite';
 import pluginReact from '@vitejs/plugin-react';
+import pluginSvgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [
     pluginReact(),
+    pluginSvgr({
+      // TODO: Configure this plugin
+      // https://react-svgr.com/docs/options/
+      // https://react-svgr.com/docs/configuration-files/
+      // https://github.com/svg/svgo#configuration
+      // svgrOptions: {},
+    }),
   ],
 });
