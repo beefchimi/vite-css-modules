8bba852425b772b73ea0061799a7da188be71b16
beefchimi <curtdulmage@gmail.com>
2022-03-06 14:23:43 -0500

    :truck: [SCSS] Organize design tokens

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // NOTE: These imports are order dependent!
 import './styles/reset.css';
 import './styles/design-system.css';
-import './styles/animation.css';
+import './styles/keyframes.css';
 import './styles/global.css';
 
 import {App} from './App';
