69332c0626d99a7c6672cf375b81e9a45b17fce3
beefchimi <curtdulmage@gmail.com>
2022-03-06 14:24:31 -0500

    :sparkles: [Utility] variationName now accepts styles

--- src/utilities/classNames.ts
+++ src/utilities/classNames.ts
@@ -10,7 +10,15 @@ export function classNames(baseName: string, variants?: Variants) {
   return [baseName, ...variantNames].join(' ');
 }
 
-export function variationName(prefix: string, variant: string) {
-  const variantFirstLetter = variant.slice(0, 1).toUpperCase();
-  return `${prefix.toLowerCase()}${variantFirstLetter}${variant.slice(1)}`;
+export function variationName(
+  prefix: string,
+  variant: string,
+  // TODO: This type may not exist in some environments,
+  // so we should make a generic CssDeclaration type.
+  styles?: CSSModuleClasses,
+) {
+  const firstLetter = variant.slice(0, 1).toUpperCase();
+  const className = `${prefix.toLowerCase()}${firstLetter}${variant.slice(1)}`;
+
+  return styles ? styles[className] : className;
 }
